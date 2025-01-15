4b827e4c3499b4dedcca185b3ba8379a5421b53a
Prish20 <contact@christadriansanya.me>
2025-01-15 15:11:16 +0300

    Add layout and main components for the homepage: Header, BookOverview, and BookList

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+
+import BookList from '@/components/BookList'
+import BookOverview from '@/components/BookOverview'
+import React from 'react'
+
+const Home = () => <>
+    <BookOverview />
+    <BookList />
+</>
+
+export default Home
