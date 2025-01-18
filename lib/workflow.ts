82f898fcd83e68b69755c9b3c964ef20c6e50d54
Prish20 <contact@christadriansanya.me>
2025-01-18 15:32:37 +0300

    Update email sender address in sendEmail function

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
             provider: resend({ token: config.env.resendToken }),
         },
         body: {
-            from: "Adrian <hello.christadriansanya.me>",
+            from: "BookWise <contact@christadriansanya.me>",
             to: [email],
             subject,
             html: message,
