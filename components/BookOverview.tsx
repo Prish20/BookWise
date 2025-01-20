237d2560cf9eb39973f02f3916f337b91482d5fd
Prish20 <contact@christadriansanya.me>
2025-01-20 20:48:15 +0300

    Fix borrowing eligibility check to handle undefined user status in BookOverview component

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -29,10 +29,8 @@ const BookOverview = async ({
         .where(eq(users.id, userId))
         .limit(1);
 
-    if (!user) return null;
-
     const borrowingEligibility = {
-        isEligible: availableCopies > 0 && user.status === "APPROVED",
+        isEligible: availableCopies > 0 && user?.status === "APPROVED",
         message:
             availableCopies <= 0
                 ? "Book is not available for borrowing"
@@ -75,11 +73,11 @@ const BookOverview = async ({
                     </p>
                 </div>
                 <p className="book-description">{description}</p>
-                <BorrowBook
+                {user && <BorrowBook
                     bookId={id}
                     userId={userId}
                     borrowingEligibility={borrowingEligibility}
-                />
+                />}
             </div>
             <div className="relative flex flex-1 justify-center">
                 <div className="relative">
